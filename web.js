6fdf693eaa1cbffcc6bc6980e2f07c464b0283c3
Happy MR <HappyMorningRain@gmail.com>
2013-07-28 15:54:13 +0000

    Reading from index file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs'); 
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var indexTextBuffer = fs.readFileSync("index.html");
+  var displayText = indexTextBuffer.toString();
+  response.send(displayText);
 });
 
 var port = process.env.PORT || 5000;
